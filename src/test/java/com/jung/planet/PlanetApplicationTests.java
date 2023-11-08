ade2309eb7ef9671d7afdf4fe37d9a34d2f5c2e0
wjdghks963 <chsw000@naver.com>
2023-11-08 20:13:46 +0900

    init

---
+++ src/test/java/com/jung/planet/PlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jung.planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
