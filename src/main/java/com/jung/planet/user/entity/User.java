fc94e18546655ce4172fe171fed83f1b97eff5d2
wjdghks963 <chsw000@naver.com>
2023-11-09 20:01:05 +0900

    modified : - User id long으로 변경

--- src/main/java/com/jung/planet/user/entity/User.java
+++ src/main/java/com/jung/planet/user/entity/User.java
@@ -17,7 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String email;
