4dc0a49fda49a244718e4998493df8d4077de3fc
wjdghks963 <chsw000@naver.com>
2023-11-09 21:15:20 +0900

    added : - DTO @NotNull 사용해 예외 처리

--- src/main/java/com/jung/planet/diary/dto/DiaryDTO.java
+++ src/main/java/com/jung/planet/diary/dto/DiaryDTO.java
@@ -2,6 +2,7 @@ package com.jung.planet.diary.dto;
 
 import com.jung.planet.diary.entity.Diary;
 import com.jung.planet.plant.entity.Plant;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +11,18 @@ import lombok.Setter;
 @Setter
 public class DiaryDTO {
 
+    @NotNull(message = "plant를 찾을 수 없습니다.")
     private Long plantId;
+
+    @NotNull(message = "제목은 꼭 필요합니다.")
     private String title;
+
+    @NotNull(message = "이미지는 꼭 필요합니다.")
     private String imgUrl;
+
+    @NotNull(message = "내용은 꼭 채워야 합니다.")
     private String content;
+
     private Boolean isPublic;
 
 
