a58259785b156d98e9dccb8c620c2f398ab2c686
wjdghks963 <chsw000@naver.com>
2023-11-09 21:16:08 +0900

    added : - get, remove 추가 - add @Valid 사용해 DTO 요소 확인, return 객체 변경

--- src/main/java/com/jung/planet/diary/controller/DiaryController.java
+++ src/main/java/com/jung/planet/diary/controller/DiaryController.java
@@ -4,14 +4,14 @@ import com.jung.planet.diary.dto.DiaryDTO;
 import com.jung.planet.diary.entity.Diary;
 import com.jung.planet.diary.service.DiaryService;
 import com.jung.planet.plant.controller.PlantController;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +22,29 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Diary> findDiary(@PathVariable("id") Long diaryId) {
+        Diary diary = diaryService.findDiary(diaryId);
+        logger.info("Diary: {}", diary);
+
+        return ResponseEntity.ok(diary);
+    }
+
+
     @PostMapping("/add")
-    public ResponseEntity<Diary> addDiary(@RequestBody DiaryDTO diaryDTO) {
+    public ResponseEntity<Map<String, Object>> addDiary(@Valid @RequestBody DiaryDTO diaryDTO) {
         logger.info("Request to add diary: {}", diaryDTO);
         Diary diary = diaryService.addDiary(diaryDTO);
         logger.info("Diary added: {}", diary);
 
-        return ResponseEntity.ok(diary);
+        return ResponseEntity.ok(Map.of("ok", true));
     }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<Map<String, Object>> removeDiary(@PathVariable("id") Long diaryId) {
+        diaryService.deleteDiary(diaryId);
+        return ResponseEntity.ok(Map.of("ok", true));
+    }
+
 
 }
