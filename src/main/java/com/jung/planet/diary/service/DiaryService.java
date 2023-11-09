9f08806a2ed894cd11ce7f34ba141cae4fb48b87
wjdghks963 <chsw000@naver.com>
2023-11-09 21:16:26 +0900

    added : - get, remove 추가

--- src/main/java/com/jung/planet/diary/service/DiaryService.java
+++ src/main/java/com/jung/planet/diary/service/DiaryService.java
@@ -5,9 +5,11 @@ import com.jung.planet.diary.entity.Diary;
 import com.jung.planet.diary.repository.DiaryRepository;
 import com.jung.planet.plant.entity.Plant;
 import com.jung.planet.plant.repository.PlantRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +19,28 @@ public class DiaryService {
     private final PlantRepository plantRepository;
 
 
+    @Transactional(readOnly = true)
+    public Diary findDiary(Long diaryId) {
+        return diaryRepository.findById(diaryId).orElseThrow(() -> new EntityNotFoundException("Diary with ID " + diaryId + " not found"));
+    }
+
+
     @Transactional
     public Diary addDiary(DiaryDTO diaryDTO) {
-        Plant plant = plantRepository.findById(diaryDTO.getPlantId()).orElseThrow(() -> new RuntimeException("Plant Not Found"));
+        Plant plant = plantRepository.findById(diaryDTO.getPlantId()).orElseThrow(() -> new EntityNotFoundException("Plant Not Found"));
         Diary diary = diaryDTO.toEntity(plant);
         return diaryRepository.save(diary);
     }
+
+    @Transactional
+    public void deleteDiary(Long diaryId) {
+        if (diaryRepository.existsById(diaryId)) {
+            diaryRepository.deleteById(diaryId);
+        } else {
+            throw new EntityNotFoundException("Diary with ID " + diaryId + " not found");
+        }
+    }
+
 
 
 }
