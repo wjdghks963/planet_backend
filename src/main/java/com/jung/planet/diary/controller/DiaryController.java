604b80f93c791a99e381231cc4be0f040b23f2fb
wjdghks963 <chsw000@naver.com>
2023-11-16 23:00:31 +0900

    refactor : - dto 수정

--- src/main/java/com/jung/planet/diary/controller/DiaryController.java
+++ src/main/java/com/jung/planet/diary/controller/DiaryController.java
@@ -1,13 +1,11 @@
 package com.jung.planet.diary.controller;
 
-import com.jung.planet.diary.dto.DiaryDTO;
 import com.jung.planet.diary.dto.DiaryDetailDTO;
-import com.jung.planet.diary.dto.request.DiaryEditDTO;
+import com.jung.planet.diary.dto.request.DiaryFormDTO;
 import com.jung.planet.diary.entity.Diary;
 import com.jung.planet.diary.service.DiaryService;
 import com.jung.planet.plant.controller.PlantController;
 import com.jung.planet.security.UserDetail.CustomUserDetails;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,9 +34,9 @@ public class DiaryController {
 
 
     @PostMapping("/add")
-    public ResponseEntity<Map<String, Object>> addDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DiaryDTO diaryDTO) {
-        logger.info("Request to add diary: {}", diaryDTO);
-        Diary diary = diaryService.addDiary(diaryDTO);
+    public ResponseEntity<Map<String, Object>> addDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DiaryFormDTO diaryFormDTO) {
+        logger.info("Request to add diary: {}", diaryFormDTO);
+        Diary diary = diaryService.addDiary(diaryFormDTO);
         logger.info("Diary added: {}", diary);
 
         return ResponseEntity.ok(Map.of("ok", true));
@@ -46,17 +44,16 @@ public class DiaryController {
 
     @DeleteMapping("/remove/{id}")
     public ResponseEntity<?> removeDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long diaryId) {
-            diaryService.deleteDiary(diaryId, customUserDetails.getUserId());
-            return ResponseEntity.ok(Map.of("ok", true));
+        diaryService.deleteDiary(diaryId, customUserDetails.getUserId());
+        return ResponseEntity.ok(Map.of("ok", true));
     }
 
 
     @PostMapping("/edit/{id}")
-    public ResponseEntity<?> editDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long diaryId, @RequestBody DiaryEditDTO diaryEditDTO) {
-        diaryService.editDiary(diaryId, diaryEditDTO,customUserDetails.getUserId());
+    public ResponseEntity<?> editDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long diaryId, @RequestBody DiaryFormDTO diaryEditDTO) {
+        diaryService.editDiary(diaryId, diaryEditDTO, customUserDetails.getUserId());
         return ResponseEntity.ok(Map.of("ok", true));
     }
-
 
 
 }
