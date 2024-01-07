2bbcfd6aa835e310dcb8ca025c5265db2ea757d6
wjdghks963 <chsw000@naver.com>
2024-01-07 15:36:49 +0900

    added : - 등급이 업그레이된 모든 유저들 확인

--- src/main/java/com/jung/planet/admin/service/AdminService.java
+++ src/main/java/com/jung/planet/admin/service/AdminService.java
@@ -1,14 +1,18 @@
 package com.jung.planet.admin.service;
 
 import com.jung.planet.admin.dto.PremiumUserDTO;
+import com.jung.planet.admin.dto.RequestUserDTO;
+import com.jung.planet.user.entity.GradeUpRequest;
 import com.jung.planet.user.entity.Subscription;
 import com.jung.planet.user.entity.SubscriptionType;
 import com.jung.planet.user.entity.User;
+import com.jung.planet.user.repository.GradeUpRequestRepository;
 import com.jung.planet.user.repository.SubscriptionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +22,7 @@ import java.util.stream.Collectors;
 public class AdminService {
 
     private final SubscriptionRepository subscriptionRepository;
+    private final GradeUpRequestRepository gradeUpRequestRepository;
 
     @Transactional(readOnly = true)
     public List<PremiumUserDTO> getAllPremiumUsers() {
@@ -34,5 +39,22 @@ public class AdminService {
         premiumUserDTO.setName(user.getName());
         premiumUserDTO.setSubscription(user.getSubscription());
         return premiumUserDTO;
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<RequestUserDTO> getAllGradeUpUsers() {
+        List<GradeUpRequest> premiumSubscriptions = gradeUpRequestRepository.findAll();
+        return premiumSubscriptions.stream()
+                .map(this::convertUserToRequestUserDTO)
+                .collect(Collectors.toList());
+    }
+
+    private RequestUserDTO convertUserToRequestUserDTO(GradeUpRequest gradeUpRequest) {
+        RequestUserDTO requestUserDTO = new RequestUserDTO();
+        requestUserDTO.setId(gradeUpRequest.getId());
+        requestUserDTO.setEmail(gradeUpRequest.getUser().getEmail());
+        requestUserDTO.setCreatedAt(gradeUpRequest.getRequestTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        return requestUserDTO;
     }
 }
